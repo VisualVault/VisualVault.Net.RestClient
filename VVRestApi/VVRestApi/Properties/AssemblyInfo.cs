34dca7d9f624bd8b053284cbaeec775752c323b9
developers@visualvault.com <tod.olsen@visualvault.com>
2018-11-03 15:43:53 -0700

    Add Nuget package manifest

--- VVRestApi/VVRestApi/Properties/AssemblyInfo.cs
+++ VVRestApi/VVRestApi/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VVRestApiNET45")]
-[assembly: AssemblyDescription("VisualVault REST API for .NET 4.5+")]
+[assembly: AssemblyTitle("VisualVault.Net.RestClient")]
+[assembly: AssemblyDescription("VisualVault REST API for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VisualVault")]
-[assembly: AssemblyProduct("VVRestApiNET45")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("VisualVault REST API for .NET")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0ad50b85-4712-47eb-b839-3eb7152a5bdf")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.15.0")]
-[assembly: AssemblyFileVersion("4.15.0.0")]
+[assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
