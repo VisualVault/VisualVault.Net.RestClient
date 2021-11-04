5c1011d48da343ed286de48c77a55936781b49ca
Fabian Montero <fabian.montero@visualvault.com>
2021-11-04 05:34:16 -0700

    Added GetCustomQueryPagedResults method to CustomQueryManager and also added sort and query parameter arguments to all GetCustomQueryResults and GetCustomQueryPagedResults methods.

--- VVRestApi/VVRestApi/Vault/CustomQueries/CustomQueryManager.cs
+++ VVRestApi/VVRestApi/Vault/CustomQueries/CustomQueryManager.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using VVRestApi.Common;
 using VVRestApi.Common.Messaging;
 using VVRestApi.Vault.Users;
+using System.Collections.Generic;
 
 namespace VVRestApi.Vault.CustomQueries
 {
@@ -18,19 +19,45 @@ namespace VVRestApi.Vault.CustomQueries
         /// <param name="queryName"></param>
         /// <param name="options"></param>
         /// <param name="filter"></param>
+        /// <param name="sortBy"></param>
+        /// <param name="sortDirection"></param>
+        /// <param name="queryParameters"></param>
         /// <returns></returns>
-        public JArray GetCustomQueryResults(string queryName, RequestOptions options = null, string filter = null)
+        public JArray GetCustomQueryResults(string queryName, RequestOptions options = null, string filter = null, string sortBy = null, string sortDirection = null, Dictionary<string,string> queryParameters = null)
         {
             if (options != null && !string.IsNullOrWhiteSpace(options.Fields))
             {
                 options.Fields = UrlEncode(options.Fields);
             }
-            var queryString = string.Format("queryName={0}", UrlEncode(queryName));
+            var queryString = "";
 
+            List<string> queryStrings = new List<string>();
+            queryStrings.Add($"queryName={UrlEncode(queryName)}");
             if (filter != null)
             {
-                queryString += $"&filter={UrlEncode(filter)}";
+                queryStrings.Add($"filter={UrlEncode(filter)}");
             }
+            if (sortBy != null)
+            {
+                queryStrings.Add($"sort={UrlEncode(sortBy)}");
+            }
+            if (sortDirection != null)
+            {
+                queryStrings.Add($"sortDir={UrlEncode(sortDirection)}");
+            }
+            if (queryParameters != null && queryParameters.Count > 0)
+            {
+                JArray queryParametersJArray = new JArray();
+                foreach (var queryParam in queryParameters)
+                {
+                    JObject queryParamJObject = new JObject();
+                    queryParamJObject["parameterName"] = queryParam.Key;
+                    queryParamJObject["value"] = queryParam.Value;
+                    queryParametersJArray.Add(queryParamJObject);
+                }
+                queryStrings.Add($"params={UrlEncode(queryParametersJArray.ToString(Newtonsoft.Json.Formatting.None))}");
+            }
+            queryString = String.Join("&", queryStrings);
 
             var results = HttpHelper.Get(GlobalConfiguration.Routes.CustomQuery, queryString, options, GetUrlParts(), this.ApiTokens, this.ClientSecrets);
             var data = results.Value<JArray>("data");
@@ -42,9 +69,12 @@ namespace VVRestApi.Vault.CustomQueries
         /// </summary>
         /// <param name="queryId"></param>
         /// <param name="options"></param>
-        /// /// <param name="filter"></param>
+        /// <param name="filter"></param>
+        /// <param name="sortBy"></param>
+        /// <param name="sortDirection"></param>
+        /// <param name="queryParameters"></param>
         /// <returns></returns>
-        public dynamic GetCustomQueryResults(Guid queryId, RequestOptions options = null, string filter = null)
+        public dynamic GetCustomQueryResults(Guid queryId, RequestOptions options = null, string filter = null, string sortBy = null, string sortDirection = null, Dictionary<string, string> queryParameters = null)
         {
             if (options != null && !string.IsNullOrWhiteSpace(options.Fields))
             {
@@ -52,14 +82,135 @@ namespace VVRestApi.Vault.CustomQueries
             }
 
             var queryString = "";
+            List<string> queryStrings = new List<string>();
             if (filter != null)
             {
-                queryString += $"filter={UrlEncode(filter)}";
+                queryStrings.Add($"filter={UrlEncode(filter)}");
             }
+            if (sortBy != null)
+            {
+                queryStrings.Add($"sort={UrlEncode(sortBy)}");
+            }
+            if (sortDirection != null)
+            {
+                queryStrings.Add($"sortDir={UrlEncode(sortDirection)}");
+            }
+            if (queryParameters != null && queryParameters.Count > 0)
+            {
+                JArray queryParametersJArray = new JArray();
+                foreach (var queryParam in queryParameters)
+                {
+                    JObject queryParamJObject = new JObject();
+                    queryParamJObject["parameterName"] = queryParam.Key;
+                    queryParamJObject["value"] = queryParam.Value;
+                    queryParametersJArray.Add(queryParamJObject);
+                }
+                queryStrings.Add($"params={UrlEncode(queryParametersJArray.ToString(Newtonsoft.Json.Formatting.None))}");
+            }
+            queryString = String.Join("&", queryStrings);
 
             var results = HttpHelper.Get(GlobalConfiguration.Routes.CustomQueryId, queryString, options, GetUrlParts(), this.ApiTokens, this.ClientSecrets, queryId);
             var data = results.Value<JArray>("data");
             return data;
+        }
+
+        /// <summary>
+        /// returns the result of a saved query as a <see cref="Page{CustomQueryResult}"/> object
+        /// </summary>
+        /// <param name="queryName"></param>
+        /// <param name="options"></param>
+        /// <param name="filter"></param>
+        /// <param name="sortBy"></param>
+        /// <param name="sortDirection"></param>
+        /// <param name="queryParameters"></param>
+        /// <returns></returns>
+        public Page<CustomQueryResult> GetCustomQueryPagedResults(string queryName, RequestOptions options = null, string filter = null, string sortBy = null, string sortDirection = null, Dictionary<string, string> queryParameters = null)
+        {
+            if (options != null && !string.IsNullOrWhiteSpace(options.Fields))
+            {
+                options.Fields = UrlEncode(options.Fields);
+            }
+
+            var queryString = "";
+            List<string> queryStrings = new List<string>();
+            queryStrings.Add($"queryName={UrlEncode(queryName)}");
+            if (filter != null)
+            {
+                queryStrings.Add($"filter={UrlEncode(filter)}");
+            }
+            if (sortBy != null)
+            {
+                queryStrings.Add($"sort={UrlEncode(sortBy)}");
+            }
+            if (sortDirection != null)
+            {
+                queryStrings.Add($"sortDir={UrlEncode(sortDirection)}");
+            }
+            if (queryParameters != null && queryParameters.Count > 0)
+            {
+                JArray queryParametersJArray = new JArray();
+                foreach (var queryParam in queryParameters)
+                {
+                    JObject queryParamJObject = new JObject();
+                    queryParamJObject["parameterName"] = queryParam.Key;
+                    queryParamJObject["value"] = queryParam.Value;
+                    queryParametersJArray.Add(queryParamJObject);
+                }
+                queryStrings.Add($"params={UrlEncode(queryParametersJArray.ToString(Newtonsoft.Json.Formatting.None))}");
+            }
+            queryString = String.Join("&", queryStrings);
+
+            var results = HttpHelper.GetPagedResult<CustomQueryResult>(GlobalConfiguration.Routes.CustomQuery, queryString, options, GetUrlParts(), this.ClientSecrets, this.ApiTokens);
+            return results;
+        }
+
+        /// <summary>
+        /// returns the result of a saved query as a <see cref="Page{CustomQueryResult}"/> object
+        /// </summary>
+        /// <param name="queryId"></param>
+        /// <param name="options"></param>
+        /// <param name="filter"></param>
+        /// <param name="sortBy"></param>
+        /// <param name="sortDirection"></param>
+        /// <param name="queryParameters"></param>
+        /// <returns></returns>
+        public Page<CustomQueryResult> GetCustomQueryPagedResults(Guid queryId, RequestOptions options = null, string filter = null, string sortBy = null, string sortDirection = null, Dictionary<string, string> queryParameters = null)
+        {
+            if (options != null && !string.IsNullOrWhiteSpace(options.Fields))
+            {
+                options.Fields = UrlEncode(options.Fields);
+            }
+
+            var queryString = "";
+            List<string> queryStrings = new List<string>();
+            if (filter != null)
+            {
+                queryStrings.Add($"filter={UrlEncode(filter)}");
+            }
+            if (sortBy != null)
+            {
+                queryStrings.Add($"sort={UrlEncode(sortBy)}");
+            }
+            if (sortDirection != null)
+            {
+                queryStrings.Add($"sortDir={UrlEncode(sortDirection)}");
+            }
+            if(queryParameters != null && queryParameters.Count > 0)
+            {
+                JArray queryParametersJArray = new JArray();
+                foreach(var queryParam in queryParameters)
+                {
+                    JObject queryParamJObject = new JObject();
+                    queryParamJObject["parameterName"] = queryParam.Key;
+                    queryParamJObject["value"] = queryParam.Value;
+                    queryParametersJArray.Add(queryParamJObject);
+                }
+                queryStrings.Add($"params={UrlEncode(queryParametersJArray.ToString(Newtonsoft.Json.Formatting.None))}");
+            }
+            queryString = String.Join("&", queryStrings);
+
+            var results = HttpHelper.GetPagedResult<CustomQueryResult>(GlobalConfiguration.Routes.CustomQueryId, queryString, options, GetUrlParts(), this.ClientSecrets, this.ApiTokens, queryId);
+            return results;
         }
 
     }
---
+++ VVRestApi/VVRestApi/Vault/CustomQueries/CustomQueryResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVRestApi.Common;
+using Newtonsoft.Json.Linq;
+
+namespace VVRestApi.Vault.CustomQueries
+{
+    public class CustomQueryResult : RestObject
+    {
+        public List<KeyValuePair<string, string>> Data { get; set; }
+
+        internal override void PopulateData(JToken data)
+        {
+            Data = new List<KeyValuePair<string, string>>();
+
+            var jobject = data as JObject;
+            if (jobject != null)
+            {
+                foreach (var dataProperty in jobject)
+                {
+                    Data.Add(new KeyValuePair<string, string>(dataProperty.Key, dataProperty.Value.ToString()));
+                }
+            }
+        }
+    }
+}
