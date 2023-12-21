0a3d857293fdac00d0adfce9894031449deb5dbd
Jon Brown <jon.brown@visualvault.com>
2023-12-21 15:38:09 -0500

    ObjectId is a string

--- VVRestApi/VVRestApi/Studio/DTO/WorkflowRunRequest.cs
+++ VVRestApi/VVRestApi/Studio/DTO/WorkflowRunRequest.cs
@@ -5,7 +5,7 @@ namespace VVRestApi.Studio.DTO
 {
     internal class WorkflowRunRequest
     {
-        public Guid ObjectId { get; set; }
+        public string ObjectId { get; set; }
         public string Reference { get; set; }
         public StudioWorkflowVariables Data { get; set; }
     }
