df716f8b9b5548dc79b303185708d5b44a45f0bd
Austin Noel <austin.noel@visualvault.com>
2024-07-03 16:09:31 -0700

    Added method to call the UploadFileByChunks REST endpoint

--- VVRestApi/VVRestApi/Vault/Library/FilesManager.cs
+++ VVRestApi/VVRestApi/Vault/Library/FilesManager.cs
@@ -94,6 +94,64 @@ namespace VVRestApi.Vault.Library
             return HttpHelper.PostMultiPart(GlobalConfiguration.Routes.Files, "", GetUrlParts(), this.ApiTokens, this.ClientSecrets, postData, fileName, fileStream);
         }
 
+
+        public JObject UploadFileByChunks(Guid documentId, string fileName, string revision, string changeReason, DocumentCheckInState checkInState, List<KeyValuePair<string, string>> indexFields, Stream fileStream, long chunkSize = 10485760)
+        {
+            JObject result = null;
+
+            if (documentId.Equals(Guid.Empty))
+            {
+                throw new ArgumentException("DocumentId is required but was an empty Guid", "documentId");
+            }
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("FileName is required but was empty", "fileName");
+            }
+
+            var jobject = new JObject();
+            foreach (var indexField in indexFields)
+            {
+                jobject.Add(new JProperty(indexField.Key, indexField.Value));
+            }
+            var jobjectString = JsonConvert.SerializeObject(jobject);
+
+            var basePostData = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("fileId", Guid.NewGuid().ToString()),
+                new KeyValuePair<string, string>("documentId", documentId.ToString()),
+                new KeyValuePair<string, string>("fileName", fileName),
+                new KeyValuePair<string, string>("revision", revision),
+                new KeyValuePair<string, string>("changeReason", changeReason),
+                new KeyValuePair<string, string>("checkInDocumentState", checkInState.ToString()),
+                new KeyValuePair<string, string>("indexFields", jobjectString)
+            };
+
+            //Calculate the chunk number based on chunk size
+            var fileLength = fileStream.Length;
+            var numberChunks = Math.Ceiling((double)fileLength / (double)chunkSize);
+            for(int i = 0; i < numberChunks; i++)
+            {
+                var postData = new List<KeyValuePair<string, string>>(basePostData)
+                {
+                    new KeyValuePair<string, string>("chunkNumber", (i + 1).ToString()),
+                    new KeyValuePair<string, string>("totalChunks", numberChunks.ToString())
+                };
+
+                var totalFileBytesRemaining = fileLength - fileStream.Position;
+                var bufferSize = Math.Min(chunkSize, totalFileBytesRemaining);
+                var buffer = new byte[bufferSize];
+                var bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+                using(var ms = new MemoryStream(buffer))
+                {
+                    result = HttpHelper.PostMultiPart(GlobalConfiguration.Routes.FilesChunk, "", GetUrlParts(), this.ApiTokens, this.ClientSecrets, postData, fileName, ms);
+                }
+            }
+
+            //Should return the final call's result
+            return result;
+        }
+
         /// <summary>
         /// Gets a file stream
         /// </summary>
